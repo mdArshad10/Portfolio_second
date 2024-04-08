271a811b70bfa4be58db9007b6d798da21637a01
mdArsahd10 <mdarshd246@gmail.com>
2024-04-08 14:22:57 +0530

    adding the loader

--- main.js
+++ main.js
@@ -1,24 +1,70 @@
-console.log("hi this is main.js");
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import SplitType from "split-type";
 
-let app = document.querySelector(".text-changer");
+gsap.registerPlugin(ScrollTrigger);
 
-var typewriter = new Typewriter(app, {
-  loop: true,
-  delay: 75,
-});
+function pageLoader() {
+  const paragraphs = document.querySelectorAll("#loader>p");
 
-typewriter
-  .pauseFor(1500)
-  .typeString("A Software engineer")
-  .pauseFor(1000)
-  .deleteAll()
-  .typeString("A Web Developer")
-  .pauseFor(1000)
-  .deleteAll()
-  .typeString("A Problem solver")
-  .pauseFor(1000)
-  .deleteAll()
-  .typeString("A fast learner")
-  .pauseFor(1000)
+  const tl = gsap.timeline();
 
-  .start();
+  paragraphs.forEach((char, i) => {
+    const text = new SplitType(char, { type: "chars" });
+
+    tl.from(
+      text.chars,
+      {
+        opacity: 0,
+        y: 80,
+        rotateX: -90,
+        stagger: 0.022,
+      },
+      "<"
+    );
+
+    tl.to(
+      text.chars,
+      {
+        opacity: 0,
+        y: -80,
+        rotateX: 90,
+        stagger: 0.022,
+      },
+      "<1"
+    );
+  });
+
+  tl.to("#loader", {
+    y:"-100vh",
+    duration: 2,
+  });
+}
+
+function textChanger() {
+  let app = document.querySelector(".text-changer");
+
+  var typewriter = new Typewriter(app, {
+    loop: true,
+    delay: 75,
+  });
+
+  typewriter
+    .pauseFor(3000)
+    .typeString("A Software engineer")
+    .pauseFor(1000)
+    .deleteAll()
+    .typeString("A Web Developer")
+    .pauseFor(1000)
+    .deleteAll()
+    .typeString("A Problem solver")
+    .pauseFor(1000)
+    .deleteAll()
+    .typeString("A fast learner")
+    .pauseFor(1000)
+    .start();
+}
+
+pageLoader();
+
+textChanger();
